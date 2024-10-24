d7ab3705f6349ae059f40bee3c558174a3c22cdb
gabrielpistore <devgbpistore@gmail.com>
2024-10-24 13:52:30 -0300

    :sparkles: add ts-comments.nvim

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,15 @@
+return {
+  -- autopairs
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+  -- ts-comments
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  }
+}
